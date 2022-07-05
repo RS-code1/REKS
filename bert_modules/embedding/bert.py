b8316bd8c786dd49af9f9ed52dac4e7e9834dcbc
Recommendation <62246464+RS-code1@users.noreply.github.com>
2022-07-05 21:36:49 +0800

    Add files via upload

---
+++ bert_modules/embedding/bert.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+import torch
+import pickle
+from .token import TokenEmbedding
+from .position import PositionalEmbedding
+
+
+class BERTEmbedding(nn.Module):
+    """
+    BERT Embedding which is consisted with under features
+        1. TokenEmbedding : normal embedding matrix
+        2. PositionalEmbedding : adding positional information using sin, cos
+        2. SegmentEmbedding : adding sentence segment info, (sent_A:1, sent_B:2)
+
+        sum of all these features are output of BERTEmbedding
+    """
+
+    def __init__(self, vocab_size, embed_size, max_len, dropout=0.1):
+        """
+        :param vocab_size: total vocab size
+        :param embed_size: embedding size of token embedding
+        :param dropout: dropout rate
+        """
+        super().__init__()
+        # self.token = TokenEmbedding(vocab_size=vocab_size, embed_size=embed_size)
+
+        self.emb_pkl = pickle.load(open('tmp/Amazon_Beauty/transe_embed.pkl', 'rb'))
+        self.token = nn.Embedding(vocab_size, embed_size)
+        self.token.weight = torch.nn.Parameter(torch.tensor(self.emb_pkl['product']))
+
+
+        self.position = PositionalEmbedding(max_len=max_len, d_model=embed_size)
+        # self.segment = SegmentEmbedding(embed_size=self.token.embedding_dim)
+        self.dropout = nn.Dropout(p=dropout)
+        self.embed_size = embed_size
+
+    def forward(self, sequence):
+        x = self.token(sequence) + self.position(sequence)  # + self.segment(segment_label)
+        return self.dropout(x)
